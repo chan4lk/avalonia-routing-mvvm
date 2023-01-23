e3587ee4342b4ffe3a44b0a5ae6041f79d23ad5a
Chandima Ranaweera <chandima@bistecglobal.com>
2023-01-23 20:56:26 +0530

    Add project files.

---
+++ Avalonia.Routing.App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Avalonia.Routing.App.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ Avalonia.Routing.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Avalonia.Routing.App.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
